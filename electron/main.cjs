7a2796af4094a342c94a598e81f886cb2af539d3
hey-ri <kwonhr01@naver.com>
2024-01-04 18:41:52 +0900

    feat: electron 앱을 시작 할 수 있게 세팅

---
+++ electron/main.cjs
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1500,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('./dist/index.html'); // <-- 요기에 './dist/'를 추가!
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: process.env.ELECTRON == 'true' ? './' : '',
+  build: {
+    watch: process.env.IS_DEV ? { include: 'src/**' } : undefined,
+  },
+  server: { port: 8000 },
+});
