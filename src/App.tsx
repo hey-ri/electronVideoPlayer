c3168ad73c70712fc17ac3d081f8339dd5335bc7
hey-ri <kwonhr01@naver.com>
2024-01-05 17:40:48 +0900

    feat: 비디오 플레이어 만들기, 썸네일 생성하기, 비디오 사용자가 선택한 것으로 표출하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 16px;
+  width: 100%;
+  height: 30px;
+  line-height: 30px;
+  background-color: #0a0a12;
+  color: #e7c00e;
+  padding-left: 8px;
+`;
+
+const Wrapper = styled.div<{ $isShowPlayList: Boolean }>`
+  width: 100%;
+  height: 100vh;
+  background-color: #34343e;
+  display: grid;
+  grid-template-columns: ${(props) => (props.$isShowPlayList ? '80% auto' : 'auto 60px')};
+`;
+const VideoWrap = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+const VideoPlayer = styled.video`
+  object-fit: contain;
+  width: 100%;
+`;
+const PlayList = styled.div`
+  background-color: #1c1c22;
+  width: 100%;
+  height: 100%;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isShowPlayList, setIsShowPlayList] = useState<Boolean>(false);
+  const [videoName, setVideoName] = useState<string>('');
+  const [thumbnail, setThumbnail] = useState<string>('');
 
+  const onShowPL = () => {
+    setIsShowPlayList((s) => (s = !s));
+  };
+
+  const addVideoPL = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files == null) return;
+
+    const videoFileInfo = e.target.files[0];
+    const videoFilePath = URL.createObjectURL(videoFileInfo);
+    console.log(videoFileInfo);
+    setVideoName(videoFilePath);
+    console.log({ videoName });
+
+    handleFileUpload(e);
+  };
+
+  const generateVideoThumbnail = (file: File) => {
+    return new Promise((resolve) => {
+      const canvas = document.createElement('canvas');
+      const video = document.createElement('video');
+
+      // this is important
+      video.autoplay = true;
+      video.muted = true;
+      video.src = URL.createObjectURL(file);
+
+      video.onloadeddata = () => {
+        let ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
+
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        if (ctx == null) return;
+        ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+        video.pause();
+        return resolve(canvas.toDataURL('image/png'));
+      };
+    });
+  };
+
+  const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files == null) return;
+    const thumbnail = await generateVideoThumbnail(e.target.files[0]);
+
+    setThumbnail(thumbnail as string);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Title>Electron Video Player</Title>
+
+      <Wrapper $isShowPlayList={isShowPlayList}>
+        <VideoWrap>
+          <VideoPlayer src={videoName} autoPlay controls muted />
+        </VideoWrap>
+        <PlayList onClick={onShowPL}>
+          <input type="file" onChange={addVideoPL} />
+          <img src={thumbnail} alt="" />
+        </PlayList>
+      </Wrapper>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
